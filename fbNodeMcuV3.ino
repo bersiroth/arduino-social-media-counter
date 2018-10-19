427edeef785882c6cef9d25b81827e091fc09dfe
bersiroth <caronbernard1@gmail.com>
2018-10-19 09:28:10 +0200

    toto

--- fbNodeMcuV3.ino
+++ fbNodeMcuV3.ino
@@ -119,19 +119,6 @@ String getTwitterFollower()
   return follower;
 }
 
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return matrix.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return matrix.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return matrix.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
 void display(String follower, Logo logo)
 {
   int followerLength = static_cast<int>(follower.length());
